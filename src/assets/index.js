7e622863646f615ada8dfc5251d1ed35ee027130
Max de Vocht <maxdevocht@hotmail.com>
2024-06-21 13:07:25 +0200

    created index.js inside assets

---
+++ src/assets/index.js
@@ -0,0 +1,90 @@
+import create from "./design/Create.png";
+import grow from "./design/Grow.png";
+import monetize from "./design/Monetize-2.png";
+import publish from "./design/Publish.png";
+import logo from "./design/logo.webp";
+import mobileHero from "./design/MobileHero.png";
+import rasterOne from "./design/raster1.svg";
+import rasterTwo from "./design/raster2.svg";
+import shaan from "./design/Shaan-1.png";
+import starOne from "./design/SVG1.webp";
+import starTwo from "./design/SVG2.webp";
+import circle from "./design/SVG3.webp";
+
+import awa from "./brands/awa.svg";
+import blockworks from "./brands/blockworks.svg";
+import bossbabe from "./brands/bossbabe.svg";
+import boston from "./brands/boston-globe-media.svg";
+import brex from "./brands/brex.svg";
+import carry from "./brands/carry-2.svg";
+import clickhole from "./brands/clickhole.svg";
+import cosmic from "./brands/CosmicRX.svg";
+import cre from "./brands/cre.svg";
+import cult from "./brands/cult-of-mac.svg";
+import daily from "./brands/daily-drop.svg";
+import exec from "./brands/exec-sum.svg";
+import fantasy from "./brands/fantasy-life.svg";
+import friday from "./brands/friday-beers.svg";
+import futureparty from "./brands/future-party.svg";
+import futurepedia from "./brands/futurepedia.svg";
+import madhappy from "./brands/madhappy.svg";
+import miss from "./brands/miss-excel.svg";
+import money from "./brands/MoneyLion.svg";
+import overtime from "./brands/ovetime.svg";
+import pump from "./brands/pump-club.svg";
+import rally from "./brands/RallyRd.svg";
+import rap from "./brands/rap-tv.svg";
+import rarible from "./brands/rarible.svg";
+import resume from "./brands/resume-worded.svg";
+import rr from "./brands/rr.svg";
+import social from "./brands/social-media-geekout.svg";
+import stocktwits from "./brands/stocktwits.svg";
+import ai from "./brands/there-is-ai-for-that.svg";
+import skinny from "./brands/theskinny.svg";
+import meme from "./brands/what-do-you-meme.svg";
+
+export {
+  create,
+  grow,
+  monetize,
+  publish,
+  logo,
+  mobileHero,
+  rasterOne,
+  rasterTwo,
+  shaan,
+  starOne,
+  starTwo,
+  circle,
+  awa,
+  blockworks,
+  bossbabe,
+  boston,
+  brex,
+  carry,
+  clickhole,
+  cosmic,
+  cre,
+  cult,
+  daily,
+  exec,
+  fantasy,
+  friday,
+  futureparty,
+  futurepedia,
+  madhappy,
+  miss,
+  money,
+  overtime,
+  pump,
+  rally,
+  rap,
+  rarible,
+  resume,
+  rr,
+  social,
+  stocktwits,
+  ai,
+  skinny,
+  meme,
+};
